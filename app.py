dd2b15d240e5b989bf6f42f73cc8b7e89a08f182
João Lucas da Silva <joaolucas200826@gmail.com>
2025-09-24 11:36:08 -0300

    Nova Versão do site

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import requests
 import json
 import BuscarCep
 import pandas as pd
-
+import BuscarCep
 
 ##### TÍTULO DA APLICAÇÃO #####
+
+st.title("Busca CEP")
 
 
 
@@ -16,10 +18,44 @@ opcoes = ["Buscar CEP", "Descobrir CEP"]
 
 
 ##### BARRA LATERAL #####
+st.sidebar.image("logo.png")
+
+opcao = st.sidebar.selectbox("Selecione Algo", opcoes)
+
+if opcao == "Buscar CEP":
+    st.image("principal.png")
+
+    ##### BOTÃO BUSCAR CEP #####
+    cep= st.text_input("Digite o CEP")
+
+    if st.button("Buscar CEP"):
+     if len(cep) != 8 or not cep.isdigit():
+                st.error("Por favor, insira um CEP válido com 8 dígitos numéricos.")
+     else:
+                try:
+                    endereco = BuscarCep.buscar_cep(cep)
+                    if endereco:
+                        st.success("Endereço encontrado:")
+                        st.write(f"CEP: {endereco[0]}")
+                        st.write(f"Endereço: {endereco[1]}")
+                        st.write(f"Bairro: {endereco[2]}")
+                        st.write(f"Cidade: {endereco[3]}")
+                        st.write(f"Estado: {endereco[4]}")
+
+                        ## Mapas
+                        st.title("Localização no Mapa")
+                        df = pd.DataFrame({"latitude": [endereco[5]], "longitude": [endereco[6]]})
+                        st.map(df, zoom=15)
+                    else:
+                        st.error("CEP não encontrado.")
+                except Exception as e:
+                    st.error(f"Ocorreu um erro ao buscar o CEP: {e}")
 
 
-##### BOTÃO BUSCAR CEP #####
+                    
+elif opcao == "Descobrir CEP":
 
-
-
-##### BOTÃO DESCOBRIR CEP #####
+    st.title("Descubra Seu CEP")
+    st.image("principal.png")
+    st.text_input("Digite seu endereço")
+    st.button("Descobrir CEP")
